4f3185d25ec8ed7f9ff567316b44204b171f4f83
Yoshio Onuki <105018988+YoshioOnuki@users.noreply.github.com>
2023-07-22 21:45:36 -0500

    Vistas de detalle de producto y carrito de compras

---
+++ app/src/main/java/com/example/proyectogrupo07/CarritoCompra.java
@@ -0,0 +1,14 @@
+package com.example.proyectogrupo07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarritoCompra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrito_compra);
+    }
+}
