633003a162f523ef0dc89a9b87801fe0d2979d1b
Yoshio Onuki <105018988+YoshioOnuki@users.noreply.github.com>
2023-07-23 22:32:07 -0500

    Detalle de producto completo

---
+++ app/src/main/java/com/example/proyectogrupo07/ApiInterface.java
@@ -0,0 +1,23 @@
+package com.example.proyectogrupo07;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+
+public interface ApiInterface {
+    @GET("producto")
+    Call<List<Producto>> getProductos();
+
+    @GET("producto/{id}")
+    Call<List<Producto>> getProductoById(@Path("id") int idProducto);
+
+    @POST("usuario/auth")
+    Call<List<Usuario>> postUsuarioLogin(@Body Login login);
+
+    @POST("usuario")
+    Call<List<Usuario>> postRegistro(@Body Usuario usuario);
+}
